c7c0013ac7c4fe4923600e8ca7513a27e7775d84
Muskan Agarwala <muskanagarwala01@gmail.com>
2025-12-30 15:50:01 +0530

    About Page Created

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
- */
+*/
+/**@type {import('next').NextConfig}*/
+
+
+
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    images:{
+        domains:["res.cloudinary.com"]
+    }
+};
 
 export default config;
